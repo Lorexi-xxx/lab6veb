1b8ea0778ee28c0a4878d9c7d9430f56738dc819
Lorexi-xxx <lorexi17pp@gmail.com>
2025-12-02 14:02:54 +0200

    Add files via upload

---
+++ survey.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Опитування</title>
+</head>
+<body>
+<h2>Анкета опитування</h2>
+
+<form action="save.php" method="POST">
+    Ім’я:<br>
+    <input type="text" name="name" required><br><br>
+
+    Email:<br>
+    <input type="email" name="email" required><br><br>
+
+    1. Який ваш улюблений предмет у навчанні?<br>
+    <select name="q1">
+        <option>Математика</option>
+        <option>Інформатика</option>
+        <option>Історія</option>
+        <option>Біологія</option>
+    </select><br><br>
+
+    2. Як часто ви користуєтеся інтернетом?<br>
+    <select name="q2">
+        <option>Кожен день</option>
+        <option>Кілька разів на тиждень</option>
+        <option>Рідко</option>
+    </select><br><br>
+
+    3. Ваш відгук про якість навчання:<br>
+    <textarea name="q3"></textarea><br><br>
+
+    <button type="submit">Надіслати</button>
+</form>
+
+</body>
+</html>
